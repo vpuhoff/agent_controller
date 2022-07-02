e32c117e9365ac4c57bb645a86c297ae30557ba3
vpuhoff <vpuhoff@live.ru>
2022-07-02 22:39:54 +0300

    Исправлен расчет потерь

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ with tqdm(initial=epoch) as pbar:
         epoch = int(resp['Value'].decode()) if resp else 0
         pbar.update(1)
         dropped = epoch-pbar.n
-        drop_percent = round(dropped/(epoch - start_epoch)*100, 4) if epoch - start_epoch > 0 else 0
+        drop_percent = round(dropped/(pbar.n - start_epoch)*100, 4) if epoch - start_epoch > 0 else 0
         pbar.desc = f"{dropped} dropped\t({drop_percent:4}%)\t{epoch}"
         if pbar.n % 100 == 0:
             dropped = 0
